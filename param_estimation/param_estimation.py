b6b627802a406858cb23717902d744ca6fb9d0eb
Dan Tran <picasdan99@gmail.com>
2022-03-01 16:41:17 +0700

    Update optimization to perform global search

--- param_estimation/param_estimation.py
+++ param_estimation/param_estimation.py
@@ -1,23 +1,23 @@
 import os
+import time
 import numpy as np
 import pandas as pd
-from typing import Any
-import numpy.typing as npt
 from param_estimation import CWD
 
-from recommendation_data_toolbox.mle import UtilityFunc, mle_estimate
+from recommendation_data_toolbox.mle import estimate_max_lik_params
 from param_estimation.utils import (
     get_probs_from_encodings,
     get_vals_from_encodings,
 )
 from recommendation_data_toolbox.utility_functions import (
-    crra_utility_fn_1param,
-    crra_utility_fn_3params,
+    UtilityFuncWrapper,
+    crra1param_utility_fn,
+    get_utility_func_wrapper,
 )
 
 
 def estimate_params_across_df(
-    df: pd.DataFrame, x0: npt.NDArray[(Any,)], utility_fn: UtilityFunc
+    df: pd.DataFrame, utility_func_wrapper: UtilityFuncWrapper
 ):
     a_values = get_vals_from_encodings(df["aValues"])
     a_probs = get_probs_from_encodings(df["aProbs"])
@@ -25,31 +25,39 @@ def estimate_params_across_df(
     b_probs = get_probs_from_encodings(df["bProbs"])
     observed_data = np.array(df["Risk"], dtype=bool)
 
-    if utility_fn == crra_utility_fn_1param:
+    if utility_func_wrapper.utility_fn is crra1param_utility_fn:
         mask = np.all(a_values >= 0, axis=1) & np.all(b_values >= 0, axis=1)
-        a_values, a_probs, b_values, b_probs, data = (
-            x[mask] for x in (a_values, a_probs, b_values, b_probs, data)
+        a_values, a_probs, b_values, b_probs, observed_data = (
+            x[mask]
+            for x in (a_values, a_probs, b_values, b_probs, observed_data)
         )
 
-    res = mle_estimate(
-        x0=x0,
-        utility_fn=utility_fn,
+    start_s = time.time()
+    res = estimate_max_lik_params(
+        utility_func_wrapper=utility_func_wrapper,
         a_values=a_values,
         a_probs=a_probs,
         b_values=b_values,
         b_probs=b_probs,
         observed_data=observed_data,
     )
+    seconds_elapsed = time.time() - start_s
     return pd.Series(
-        [res.success, res.fun, res.x],
-        index=["isSuccess", "minNegLogLik", "optimalParams"],
+        [
+            res.success,
+            res.fun,
+            res.x,
+            res.nit,
+            seconds_elapsed,
+        ],
+        index=[
+            "isSuccess",
+            "minNegLogLik",
+            "optimalParams",
+            "iterNum",
+            "secondsElapsed",
+        ],
     )
-
-
-UTILITY_MODELS = {
-    "crra1param": dict(utility_fn=crra_utility_fn_1param, params_num=1),
-    "crra3params": dict(utility_fn=crra_utility_fn_3params, params_num=3),
-}
 
 
 def estimate_params_by_subjects(filename: str, utility_model: str):
@@ -58,6 +66,5 @@ def estimate_params_by_subjects(filename: str, utility_model: str):
     df = pd.read_csv(filepath)
     return df.groupby("SubjID").apply(
         estimate_params_across_df,
-        x0=np.ones((UTILITY_MODELS[utility_model]["params_num"],)),
-        utility_fn=UTILITY_MODELS[utility_model]["utility_fn"],
+        utility_func_wrapper=get_utility_func_wrapper(utility_model),
     )
--- recommendation_data_toolbox/mle.py
+++ recommendation_data_toolbox/mle.py
@@ -1,13 +1,14 @@
 import numpy as np
 from scipy.stats import norm
-from scipy.optimize import minimize
-from typing import Any, Callable, Union
+from scipy.optimize import basinhopping
+import random
+from typing import List, Tuple
 import numpy.typing as npt
 
-UtilityFunc = Callable[
-    [tuple, Union[np.int_, npt.NDArray[np.int_]]],
-    Union[np.float64, npt.NDArray[np.float64]],
-]
+from recommendation_data_toolbox.utility_functions import (
+    UtilityFunc,
+    UtilityFuncWrapper,
+)
 
 
 def eu_fn(
@@ -16,7 +17,7 @@ def eu_fn(
     values: npt.NDArray[np.int_],
     probs: npt.NDArray[np.float64],
 ) -> np.float64:
-    return np.sum(probs * utility_fn(params, values), axis=-1)
+    return np.sum(np.multiply(probs, utility_fn(params, values)), axis=-1)
 
 
 def neg_log_lik_fn(
@@ -30,25 +31,39 @@ def neg_log_lik_fn(
         np.bool_
     ],  # 0 if option A was chosen, 1 otherwise
 ):
-    eu_deltas = eu_fn(params, utility_fn, a_values, a_probs) - eu_fn(
-        params, utility_fn, b_values, b_probs
+    eu_deltas = np.subtract(
+        eu_fn(params, utility_fn, a_values, a_probs),
+        eu_fn(params, utility_fn, b_values, b_probs),
     )
     signs = observed_data * -2 + 1  # 1 if option A was chosen, -1 otherwise
-    return -np.log(norm.cdf(eu_deltas * signs)).sum(axis=-1)
+    return -np.log(norm.cdf(np.multiply(eu_deltas, signs))).sum(axis=-1)
 
 
-def mle_estimate(
-    x0: npt.NDArray[(Any,)],
-    utility_fn: UtilityFunc,
+def get_initial_params(bounds: List[Tuple[np.float64, np.float64]]):
+    return np.array([random.uniform(*bound) for bound in bounds])
+
+
+def estimate_max_lik_params(
+    utility_func_wrapper: UtilityFuncWrapper,
     a_values: npt.NDArray[np.int_],
     a_probs: npt.NDArray[np.float64],
     b_values: npt.NDArray[np.int_],
     b_probs: npt.NDArray[np.float64],
     observed_data: npt.NDArray[np.bool_],
 ):
-    return minimize(
-        fun=neg_log_lik_fn,
-        x0=x0,
-        args=(utility_fn, a_values, a_probs, b_values, b_probs, observed_data),
-        method="Nelder-Mead",
+    return basinhopping(
+        func=neg_log_lik_fn,
+        x0=utility_func_wrapper.get_random_params(),
+        minimizer_kwargs=dict(
+            bounds=utility_func_wrapper.bounds,
+            method="Nelder-Mead",
+            args=(
+                utility_func_wrapper.utility_fn,
+                a_values,
+                a_probs,
+                b_values,
+                b_probs,
+                observed_data,
+            ),
+        ),
     )

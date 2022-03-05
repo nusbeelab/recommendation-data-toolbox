0ccca68c4a8008003796362868bfe69c24ab2bc3
Dan Tran <58513950+picasdan9@users.noreply.github.com>
2022-03-05 08:06:58 +0700

    Set up CI (#16)
    * Set up CI
    
    * Update requirements-dev.txt
    
    * Fix failing tests
    
    * Update CI config file

--- recommendation_data_toolbox/tests/models/test_lottery_utility.py
+++ recommendation_data_toolbox/tests/models/test_lottery_utility.py
@@ -1,42 +1,39 @@
 import unittest
 import numpy as np
 
-from recommendation_data_toolbox.lottery import Lottery
 from recommendation_data_toolbox.models.lottery_utility import (
     get_lottery_utility_model,
 )
 
 
 class TestLotteryUtility(unittest.TestCase):
-    lottery = None
-    nonneg_outcome_lotery = None
+    outcomes = None
+    outcomes_nonneg = None
+    probs = None
 
     @classmethod
     def setUpClass(cls):
-        cls.lottery = Lottery(
-            np.array([30, 20, -10]), np.array([0.2, 0.3, 0.5])
-        )
-        cls.nonneg_outcome_lotery = Lottery(
-            np.array([30, 20, 10]), np.array([0.2, 0.3, 0.5])
-        )
+        cls.outcomes = np.array([30, 20, -10])
+        cls.outcomes_nonneg = np.array([30, 20, 10])
+        cls.probs = np.array([0.2, 0.3, 0.5])
 
-    def test_expectedUtility_1paramOutcomeUtility(self):
+    def test_expectedUtility_powerNonnegOutcomeUtility(self):
         actual = get_lottery_utility_model(
-            "expected_utility", "1param"
-        ).lottery_utility_func((0.3,), self.nonneg_outcome_lotery)
+            "expected_utility", "power_nonneg"
+        ).lottery_utility_func((0.3,), self.outcomes_nonneg, self.probs)
         expected = 2.28940619609
         self.assertTrue(np.allclose(actual, expected))
 
-    def test_expectedUtility_3paramsOutcomeUtility(self):
+    def test_expectedUtility_powerOutcomeUtility(self):
         actual = get_lottery_utility_model(
-            "expected_utility", "3params"
-        ).lottery_utility_func((0.4, 1.5, 0.6), self.lottery)
+            "expected_utility", "power"
+        ).lottery_utility_func((0.4, 1.5, 0.6), self.outcomes, self.probs)
         expected = -1.21185560577
         self.assertTrue(np.allclose(actual, expected))
 
     def test_prospectTheory(self):
         actual = get_lottery_utility_model(
             "prospect_theory"
-        ).lottery_utility_func((0.3, 0.5), self.nonneg_outcome_lotery)
+        ).lottery_utility_func((0.3, 0.5), self.outcomes_nonneg, self.probs)
         expected = 2.09671782936
         self.assertTrue(np.allclose(actual, expected))

f12aa7afab5b269fd4c93d8ac061d13c72b53672
Dan Tran <picasdan99@gmail.com>
2022-03-10 16:25:19 +0700

    Fix error in naming UBCF

--- recommendation_data_toolbox/rec/cf/neighborhood_based.py
+++ recommendation_data_toolbox/rec/cf/neighborhood_based.py
@@ -7,9 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from recommendation_data_toolbox.rec import Recommender
 
 
-class UcbfRecommender(Recommender):
-    """A user-based collaborative filtering (UBCF) recommender."""
-
+class NbcfRecommender(Recommender):
     def __init__(
         self,
         rating_matrix: npt.NDArray[np.int_],
@@ -33,6 +31,10 @@ class UcbfRecommender(Recommender):
         self.knn_classifier = KNeighborsClassifier(
             n_neighbors=n_neighbors, weights="distance", metric="manhattan"
         )
+
+
+class UbcfRecommender(NbcfRecommender):
+    """A user-based collaborative filtering (UBCF) recommender."""
 
     def rec(self, lot_pair_id: int):
         X = self.rating_matrix[:, self.subj_lot_pair_ids]
@@ -43,30 +45,8 @@ class UcbfRecommender(Recommender):
         return self.knn_classifier.predict([self.subj_decisions])[0]
 
 
-class IbcfRecommender(Recommender):
-    def __init__(
-        self,
-        rating_matrix: npt.NDArray[np.int_],
-        subj_lot_pair_ids: npt.NDArray[np.int_],
-        subj_decisions: npt.NDArray[np.int_],
-        n_neighbors: Optional[int] = 5,
-    ):
-        """
-        Parameters
-        ----------
-        rating_matrix : 2D-array of boolean
-            A full rating matrix from pre-experiment. Each row represents the decisions
-            of a subject for lot_pair_ids 0..(n-1).
-        """
-        self.rating_matrix = rating_matrix
-
-        order = subj_lot_pair_ids.argsort()
-        self.subj_lot_pair_ids = subj_lot_pair_ids[order]
-        self.subj_decisions = subj_decisions[order]
-
-        self.knn_classifier = KNeighborsClassifier(
-            n_neighbors=n_neighbors, weights="distance", metric="manhattan"
-        )
+class IbcfRecommender(NbcfRecommender):
+    """An item-based collaborative filtering (IBCF) recommender."""
 
     def rec(self, lot_pair_id: int):
         X = self.rating_matrix[:, self.subj_lot_pair_ids].T
